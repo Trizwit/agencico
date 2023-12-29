0fa57ee2a61c60502459c3ab3520e53bd682dde3
Gayathri AV <gaya3kousthubam@gmail.com>
2023-12-29 10:23:48 +0530

    added certificate

--- components/header/hbottom.js
+++ components/header/hbottom.js
@@ -38,26 +38,18 @@ return(
 <div className="hrrd-hero-img">
     <img src={HOMEHEADER.ConverImage} alt=""/>
         <div className="hrrd-hero-post">
-            <h4>Post Type</h4>
+            <h4>Verify</h4>
             <div className="hrrd-hero-post-shape">
                 <img src="assets/images/shape/hrrd-hero-post.png" alt=""/>
                 <Link href="/"><a><i className="bi bi-download"></i></a></Link>
             </div>
             <ul>
-                <li><span></span><Link href="/"><a>Images</a></Link></li>
-                <li><span></span><Link href="/"><a>Videos</a></Link></li>
+                <li><span></span><Link href="/"><a>Certificate</a></Link></li>
             </ul>
         </div>
-        <div className="like-cart hero-like-cart">
-            <div className="like-cart-box">
-                <div className="like-cart-icon">
-                    <i className="bx bxs-like"></i>
-                </div>
-                <div className="like-cart-content">
-                    <h4>398K</h4>
-                    <h6>Total Likes</h6>
-                </div>
-            </div>
+        <div>
+            <img className="like-cart hero-like-cart" src="assets/images/shape/hrrd-hero-post2.png" alt=""/>
+           
         </div>
 </div>
 </div>
@@ -74,7 +66,7 @@ return(
     <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
         <div className="hrrd-hero-img-wrap-mobile">
         <div className="hrrd-hero-img">
-            <img src="assets/images/hrrd-hero-img-mobile.png" alt=""/>
+            <img src="assets/images/hrrd-hero-img.png" alt=""/>
         </div>
     </div>
     </div>
