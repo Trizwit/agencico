09797851525da4319a42294b0dff63ec2093a9d0
Gayathri AV <gaya3kousthubam@gmail.com>
2024-01-03 11:43:39 +0530

    padding

--- components/home/NewsLater.js
+++ components/home/NewsLater.js
@@ -7,8 +7,8 @@ return(
 <div className="row">
 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
     <div className="hrrd-subscribe-wrap">
-                <div className="hrdd-section-title hrrd-subscribe-title text-center wow animate fadeInDown" data-wow-delay="100ms" data-wow-duration="1500ms">
-                    <h1>“Explore the future of credentialing with us — where security, efficiency, and innovation converge to redefine how we recognize and validate achievements.”</h1>
+                <div className=" testimonial-title hrdd-section-title hrrd-subscribe-title text-center wow animate fadeInDown" data-wow-delay="100ms" data-wow-duration="1500ms">
+                    <h1>Explore the future of credentialing with us — where security, efficiency, and innovation converge to redefine how we recognize and validate achievements.</h1>
                 </div>
     </div>
 </div>
