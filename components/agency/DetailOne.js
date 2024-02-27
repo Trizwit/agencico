94dba1536503f45015703afea6ffc9a904d8ffed
binilvjacob <itsmebinilvjacob@gmail.com>
2024-02-27 09:30:19 +0530

    hero gif updated

--- components/agency/DetailOne.js
+++ components/agency/DetailOne.js
@@ -1,79 +1,96 @@
-import Link from "next/link"
-import AgencyConstantTwo from "../../constant/Agency/DetailTwo"
+import Link from "next/link";
+import AgencyConstantTwo from "../../constant/Agency/DetailTwo";
 
-const AgencyDetail=()=>{
-return <>
-<div className="ag-working-process">
-<div className="container">
-<div className="row align-items-center gy-4">
-<div className="col-xxl-7 col-lg-6">
-    <div className="position-relative">
-        <div className="ag-process-features-image d-inline-block position-relative">
-            <img src={AgencyConstantTwo.CoverImg1} alt="" className="position-relative"/>
-            
-            <div className="process-animi-shape position-absolute d-lg-block d-none">
-                <img src="/assets/images/agency/shapes/process-shape1.png" alt=""/>
+const AgencyDetail = () => {
+  return (
+    <>
+      <div className="ag-working-process">
+        <div className="container">
+          <div className="row align-items-center gy-4">
+            <div className="col-xxl-7 col-lg-6">
+              <div className="position-relative">
+                <div className="ag-process-features-image d-inline-block position-relative">
+                  <img
+                    src={AgencyConstantTwo.CoverImg1}
+                    alt=""
+                    className="position-relative"
+                  />
+
+                  <div className="process-animi-shape position-absolute d-lg-block d-none">
+                    <img
+                      src="/assets/images/agency/shapes/process-shape1.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="features-img-bg">
+                  <img
+                    src="/assets/images/agency/shapes/process-circle1.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
             </div>
+            <div className="col-xxl-5 col-lg-6 pt-5 pt-lg-0">
+              <div className="ag-process-disc">
+                <h2 className="ag-section-title text-start">
+                  {AgencyConstantTwo.Title1}
+                </h2>
 
-           
-        </div>
-        <div className="features-img-bg">
-            <img src="/assets/images/agency/shapes/process-circle1.png" alt=""/>
-        </div>
-    </div>
-    
-</div>   
-<div className="col-xxl-5 col-lg-6 pt-5 pt-lg-0">
-    <div className="ag-process-disc">
-        <h2 className="ag-section-title text-start">
-            {AgencyConstantTwo.Title1}
-        </h2>
-        
-            <p>{AgencyConstantTwo.subTitle1}</p>
+                <p>{AgencyConstantTwo.subTitle1}</p>
 
-            <div className="process-btn">
-                <Link href="/" className="ag-btn-fill">
-
+                <div className="process-btn">
+                  <Link href="/" className="ag-btn-fill">
                     {AgencyConstantTwo.actionBtn1}
-                </Link>
+                  </Link>
+                </div>
+              </div>
             </div>
-    </div>
-</div>
-</div>
-<div className="row align-items-center process-row-gap gy-4">
-<div className="col-xxl-5 col-lg-6  order-1 order-lg-0 pt-5 pt-lg-0">
-    <div className="ag-process-disc">
-        <h2 className="ag-section-title text-start">
-            {AgencyConstantTwo.Title2}
-        </h2>
-        
-        <p>{AgencyConstantTwo.subTitle2}</p>
+          </div>
+          <div className="row align-items-center process-row-gap gy-4">
+            <div className="col-xxl-5 col-lg-6  order-1 order-lg-0 pt-5 pt-lg-0">
+              <div className="ag-process-disc">
+                <h2 className="ag-section-title text-start">
+                  {AgencyConstantTwo.Title2}
+                </h2>
 
-        <div className="process-btn">
-            <Link href="/" className="ag-btn-fill">{AgencyConstantTwo.actionBtn2}</Link>
-        </div>
-    </div>
-</div>
-<div className="col-xxl-7 col-lg-6 order-0 order-lg-1">
-    <div className="position-relative text-lg-end">
-        <div className="ag-process-features-image  d-inline-block position-relative">
-            <img src={AgencyConstantTwo.CoverImg2} alt="" className="position-relative"/>
-            
-            <div className="process-animi-shape position-absolute d-lg-block d-none">
-                <img src="/assets/images/agency/shapes/process-shape2.png" alt=""/>
+                <p>{AgencyConstantTwo.subTitle2}</p>
+
+                <div className="process-btn">
+                  <Link href="/" className="ag-btn-fill">
+                    {AgencyConstantTwo.actionBtn2}
+                  </Link>
+                </div>
+              </div>
             </div>
+            <div className="col-xxl-7 col-lg-6 order-0 order-lg-1">
+              <div className="position-relative text-lg-end">
+                <div className="ag-process-features-image  d-inline-block position-relative">
+                  <img
+                    src={AgencyConstantTwo.CoverImg2}
+                    alt=""
+                    className="position-relative"
+                  />
 
-           
+                  <div className="process-animi-shape position-absolute d-lg-block d-none">
+                    <img
+                      src="/assets/images/agency/shapes/process-shape2.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="features-img-bg2">
+                  <img
+                    src="assets/images/agency/shapes/process-circle2.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="features-img-bg2">
-            <img src="assets/images/agency/shapes/process-circle2.png" alt=""/>
-        </div>
-    </div>
-</div>
-
-</div>
-</div>
-</div>
-</>;
-}
-export default AgencyDetail
+      </div>
+    </>
+  );
+};
+export default AgencyDetail;
--- components/header/agencyBottom.js
+++ components/header/agencyBottom.js
@@ -1,50 +1,46 @@
-import Link from "next/link"
-import AgencyConstantOne from "../../constant/Agency/DetailOne"
+import Link from "next/link";
+import AgencyConstantOne from "../../constant/Agency/DetailOne";
 
-const AgencyBottom=()=>{
-return <>
-<div className="ag-hero-style">
-<div className="container">
-<div className="row justify-content-lg-between justify-content-center">
-    <div className="col-lg-5 order-1 order-lg-0">
-        <div className="ag-hero-content">
-            <h2 className="ag-hero-title">
-                {AgencyConstantOne.Title}
-            </h2>
-            <p className="test">
-                {AgencyConstantOne.subTitle}
-            </p>
-            <div className="ag-hero-btn">
-                <Link href="/" className="ag-btn-fill">{AgencyConstantOne.actionBtn}</Link>
-            </div>
-        </div>
-    </div>
-
-    <div className="col-lg-7 col-md-10 order-0 order-lg-1 d-flex align-items-end justify-content-lg-end justify-content-center">
-        <div className="ag-hero-figure">
-            <img src="/assets/images/agency/ag-hero-figure.png" alt=""/>
-
-
-
-            <div className="review-label">
-                <div className="d-flex align-items-center label-header">
-                    <div className="client-avater">
-                        <img src={AgencyConstantOne.reviewLabel.pic} alt=""/>
-                    </div>                  
+const AgencyBottom = () => {
+  return (
+    <>
+      <div className="ag-hero-style">
+        <div className="container">
+          <div className="row justify-content-lg-between justify-content-center">
+            <div className="col-lg-5 order-1 order-lg-0">
+              <div className="ag-hero-content">
+                <h2 className="ag-hero-title">{AgencyConstantOne.Title}</h2>
+                <p className="test">{AgencyConstantOne.subTitle}</p>
+                <div className="ag-hero-btn">
+                  <Link href="/" className="ag-btn-fill">
+                    {AgencyConstantOne.actionBtn}
+                  </Link>
                 </div>
-
-                <h5>{AgencyConstantOne.reviewLabel.title}</h5>
+              </div>
             </div>
-        </div>
-    </div>
-</div>
-</div>
 
-<div className="ag-hero-random-shapes d-none d-xl-block">
-<img src="/assets/images/agency/shapes/shape-1.svg" className="shape-one" alt=""/>
-<img src="/assets/images/agency/shapes/shape-2.svg" className="shape-two" alt=""/>
-</div>
-</div>
-</>;
-}
-export default AgencyBottom
+            <div className="col-lg-7 col-md-10 order-0 order-lg-1 d-flex align-items-end justify-content-lg-end justify-content-center">
+              <div className="ag-hero-figure">
+                <img src="/assets/images/agency/ag-hero-figure.gif" alt="" />{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="ag-hero-random-shapes d-none d-xl-block">
+          <img
+            src="/assets/images/agency/shapes/shape-1.svg"
+            className="shape-one"
+            alt=""
+          />
+          <img
+            src="/assets/images/agency/shapes/shape-2.svg"
+            className="shape-two"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+export default AgencyBottom;
